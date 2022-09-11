26ed2dd0cb79c0ed4c60d9568a15ebeac73d9aca
rinehaxor <rinehaxor@users.noreply.github.com>
2022-09-11 15:19:29 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 //import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/db_staycation');
+mongoose.connect('mongodb+srv://rinehaxor:2133qwe1@cluster0.wqfex5v.mongodb.net/db_staycation?retryWrites=true&w=majority');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 // Connect to MongoDB via Mongoose
 seeder.connect(
-  'mongodb://127.0.0.1:27017/db_staycation',
+  'mongodb+srv://rinehaxor:2133qwe1@cluster0.wqfex5v.mongodb.net/db_staycation?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useCreateIndex: true,
